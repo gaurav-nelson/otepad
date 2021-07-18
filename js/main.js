fe7813a7cead3171e4ff90fe64440f63bb590cc1
Gaurav Nelson <ganelson@outlook.com>
2021-07-18 20:36:45 +1000

    fix theme switching

--- js/main.js
+++ js/main.js
@@ -1,73 +1,96 @@
+var editDiv = document.getElementById("editable");
+
 window.onload = function () {
-    setEndOfContenteditable(document.getElementById("editable"));
-  };
+  editDiv.contentEditable = true;
+  setEndOfContenteditable(editDiv);
+};
 
-  function setEndOfContenteditable(contentEditableElement) {
-    var range, selection;
-    if (document.createRange) {
-      //Firefox, Chrome, Opera, Safari, IE 9+
-      range = document.createRange(); //Create a range (a range is a like the selection but invisible)
-      range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
-      range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
-      selection = window.getSelection(); //get the selection object (allows you to change selection)
-      selection.removeAllRanges(); //remove any selections already made
-      selection.addRange(range); //make the range you have just created the visible selection
-    } else if (document.selection) {
-      //IE 8 and lower
-      range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
-      range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
-      range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
-      range.select(); //Select the range (make it the visible selection
-    }
+function setEndOfContenteditable(contentEditableElement) {
+  var range, selection;
+  if (document.createRange) {
+    //Firefox, Chrome, Opera, Safari, IE 9+
+    range = document.createRange(); //Create a range (a range is a like the selection but invisible)
+    range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
+    range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+    selection = window.getSelection(); //get the selection object (allows you to change selection)
+    selection.removeAllRanges(); //remove any selections already made
+    selection.addRange(range); //make the range you have just created the visible selection
+  } else if (document.selection) {
+    //IE 8 and lower
+    range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
+    range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
+    range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+    range.select(); //Select the range (make it the visible selection
+  }
+}
+
+function themeSwitch() {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
   }
 
-  function themeSwitch() {
-    document.body.classList.toggle("dark-theme");
-    setEndOfContenteditable(document.getElementById("editable"));
-  }
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+  setEndOfContenteditable(editDiv);
+}
 
-  function storageAvailable(type) {
-    var storage;
-    try {
-      storage = window[type];
-      var x = "__storage_test__";
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-    } catch (e) {
-      return (
-        e instanceof DOMException &&
+function storageAvailable(type) {
+  var storage;
+  try {
+    storage = window[type];
+    var x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
         // everything except Firefox
-        (e.code === 22 ||
-          // Firefox
-          e.code === 1014 ||
-          // test name field too, because code might not be present
-          // everything except Firefox
-          e.name === "QuotaExceededError" ||
-          // Firefox
-          e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-        // acknowledge QuotaExceededError only if there's something already stored
-        storage &&
-        storage.length !== 0
-      );
-    }
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
   }
+}
 
-  if (storageAvailable("localStorage")) {
-    // Yippee! We can use localStorage awesomeness
-    if (localStorage.getItem("content")) {
-      loadContent();
-    }
-  } else {
-    // Too bad, no localStorage for us
+if (storageAvailable("localStorage")) {
+  if (localStorage.getItem("content")) {
+    loadContent();
   }
-
-  function loadContent(){
-      document.getElementById('editable').innerHTML = localStorage.getItem("content");
+  var storedTheme =
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
+  if (storedTheme) {
+    document.documentElement.setAttribute("data-theme", storedTheme);
   }
+} else {
+  // no localStorage available, so we need to set the theme manually
+  document.documentElement.setAttribute("data-theme", "light");
+}
 
-  function saveContent(){
-      localStorage.setItem('content', document.getElementById('editable').innerHTML);
-  }
+function loadContent() {
+  editDiv.innerHTML =
+    localStorage.getItem("content");
+}
 
-  document.getElementById('editable').addEventListener("blur", saveContent);
+function saveContent() {
+  localStorage.setItem(
+    "content",
+    editDiv.innerHTML
+  );
+}
+
+editDiv.addEventListener("blur", saveContent);
